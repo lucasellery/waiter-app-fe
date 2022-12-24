f704f3c1b05a8cb207e3f7e1887a754dd76e141b
lucasellery <olucasellery@gmail.com>
2022-12-23 23:33:15 -0300

    first commit | created web app

---
+++ src/components/OrderModal/styles.ts
@@ -0,0 +1,167 @@
+import styled from 'styled-components';
+
+export const Overlay = styled.div`
+  width: 100%;
+  height: 100%;
+  left: 0px;
+  top: 0px;
+  background: rgba(0, 0, 0, 0.8);
+  backdrop-filter: blur(4.5px);
+
+  position: fixed;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ModalBody = styled.div`
+  background: #fff;
+  width: 480px;
+
+  border-radius: 8px;
+  padding: 2rem;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    strong {
+      font-size: 1.5rem;
+    }
+
+    button {
+      border: 0;
+      background: transparent;
+
+      line-height: 0;
+    }
+  }
+
+  .status-container {
+    margin-top: 32px;
+
+    small {
+      font-size: 14px;
+      opacity: 0.8;
+    }
+
+    div {
+      display: flex;
+      align-items: center;
+
+      gap: 0.5rem;
+
+      margin-top: 0.5rem;
+    }
+  }
+`;
+
+export const OrderDetails = styled.div`
+  margin-top: 2rem;
+
+  > strong {
+    font-weight: 500;
+    font-size: 14px;
+    opacity: 0.8;
+  }
+
+  .order-items {
+    margin-top: 1rem;
+
+    .item {
+      display: flex;
+
+      & + .item {
+        margin-top: 1rem;
+      }
+
+      img {
+        border-radius: 6px;
+      }
+
+      .quantity {
+        font-size: 14px;
+        color: #666;
+
+        display: block;
+        min-width: 20px;
+
+        margin-left: 12px;
+      }
+
+      .product-details {
+        margin-left: 4px;
+
+        strong {
+          display: block;
+          margin-bottom: 4px;
+        }
+
+        span {
+          font-size: 14px;
+          color: #666;
+        }
+      }
+    }
+  }
+
+  .total {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 24px;
+
+    span {
+      font-weight: 500;
+      font-size: 14px;
+      opacity: 0.8;
+    }
+
+    strong {
+      font-size: 1rem;
+    }
+  }
+`;
+
+export const Actions = styled.footer`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  margin-top: 32px;
+
+  .primary {
+    background-color: #333333;
+    border-radius: 48px;
+    border: 0;
+    color: #fff;
+    padding: 12px 24px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+
+    transition: all 250ms ease-out;
+
+    &:hover {
+      filter: brightness(120%) saturate(120%);
+    }
+  }
+
+  .secondary {
+    padding: 14px 24px;
+    color: #D73035;
+    font-weight: bold;
+    background-color: transparent;
+    border: 0;
+
+    transition: all 250ms ease-out;
+
+    &:hover {
+      filter: brightness(120%) saturate(120%);
+    }
+  }
+`;
---
+++ src/components/OrdersBoard/index.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { Order } from '../../types/Order';
+import { OrderModal } from '../OrderModal';
+import { Board, OrdersContainer } from './styles';
+
+interface OrdersBoardProps {
+  icon: string;
+  title: string;
+  orders: Order[];
+}
+
+export function OrdersBoard({ icon, title, orders }: OrdersBoardProps) {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [selectedOrder, setSelectedOrder] = useState<null | Order>(null);
+
+  function handleOpenModal(order: Order) {
+    setIsModalVisible(true);
+    setSelectedOrder(order);
+  }
+
+  function handleCloseModal() {
+    setIsModalVisible(false);
+    setSelectedOrder(null);
+  }
+
+  return (
+    <Board>
+      <header>
+        <span>{icon}</span>
+        <strong>{title}</strong>
+        <span>({orders?.length})</span>
+      </header>
+
+      {orders.length > 0 && (
+        <OrdersContainer>
+          {orders.map((order) => (
+            <button key={order._id} type="button" onClick={() => handleOpenModal(order)}>
+              <strong>Mesa {order?.table}</strong>
+              <span>{order?.products?.length} itens</span>
+            </button>
+          ))}
+        </OrdersContainer>
+      )}
+
+      <OrderModal
+        visible={isModalVisible}
+        order={selectedOrder}
+        onClose={handleCloseModal}
+      />
+    </Board>
+  );
+}
