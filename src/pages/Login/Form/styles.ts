6dcafd0e6ae720d2d049a800a02a4dce12733d26
lucasellery-osf <lucas.ellery@osf.digital>
2023-02-20 16:08:58 -0300

    (feat) login UI page finished

--- src/pages/Login/Form/styles.ts
+++ src/pages/Login/Form/styles.ts
@@ -13,10 +13,32 @@ export const Label = styled.label`
   line-height: 150%;
 `;
 
+export const PasswordContainer = styled.div`
+  margin-bottom: 2rem;
+`;
+
+export const PasswordInputContainer = styled.div`
+  .password-container {
+    position: relative;
+  }
+
+  .password {
+    padding-right: 30px; /* Adicione um espaço à direita do campo para o ícone */
+  }
+
+  .password-toggle-icon {
+    position: absolute;
+    top: 60%; /* Centralize o ícone verticalmente */
+    right: 15px; /* Alinhe o ícone à direita */
+    transform: translateY(-50%); /* Centralize o ícone verticalmente */
+    cursor: pointer;
+  }
+`;
+
 export const Input = styled.input`
   padding: 0px 16px;
   height: 56px;
-  border: 1px solid #CCCCCC;
+  border: 1px solid;
   border-radius: 8px;
 
   font-weight: 400;
@@ -26,7 +48,17 @@ export const Input = styled.input`
   color: #999999;
 
   width: 100%;
+  margin-top: 8px;
+`;
 
-  margin-bottom: 2rem;
+export const WarningLabel = styled.p`
+  color: #D73035;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 150%;
+
+  align-items: center;
+  display: flex;
+  gap: 8px;
   margin-top: 8px;
 `;
